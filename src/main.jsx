a96ec1835d1fcbd20cee8032ec6264d4cf303cc4
rafaellabastos1 <rm552425@fiap.com.br>
2023-10-03 15:21:36 -0300

    arrumei as rotas da main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,45 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-
-//BLOCO DE ROTAS
+//ROTAS
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import EditarProdutos from './routes/EditarProdutos.jsx';
 import Erro404 from './routes/Erro404.jsx';
 import Home from './routes/Home.jsx';
 import Produtos from './routes/Produtos.jsx';
 import ExcluirProdutos from './routes/ExcluirProdutos.jsx';
+import InserirProduto from './routes/InserirProduto.jsx';
 
 const router = createBrowserRouter([
-  { path: '/', 
+  { 
+    path: '/', 
     element: <App/>,
     errorElement: <Erro404/>,
-    children: 
-    [
+    children: [
       { 
         path: '/', 
-        element: <Home />
+        element: <Home/>
       },
       { 
         path: '/produtos', 
-        element: <Produtos />
+        element: <Produtos/>
       },
       { 
         path: '/editar/produtos/:id', 
-        element: <EditarProdutos />
+        element: <EditarProdutos/>
       },
       { 
         path: '/excluir/produtos/:id', 
         element: <ExcluirProdutos/>
       },
+      { 
+        path: '/cadastrar', 
+        element: <InserirProduto/>
+      },
     ]
   }
 ]);
 
-//BLOCO DE ROTAS
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
    <RouterProvider router={router}/>
-  // </React.StrictMode>,
 )
