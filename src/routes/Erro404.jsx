4f50f140fbfa8e4b250cac766460962363f62ebf
rafaellabastos1 <rm552425@fiap.com.br>
2023-10-03 15:05:09 -0300

    arrumando o export default do Erro404

--- src/routes/Erro404.jsx
+++ src/routes/Erro404.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom"
 
-export default function Erro404() {
+function Erro404() {
   return (
     <div>
         <h1>Esta página não existe</h1>
-        <p>Retorne para o início <span> <Link to="/">INÍCIO</Link> </span></p>
+        <p>Retorne para o início <span><Link to="/">INÍCIO</Link></span></p>
     </div>
   )
 }
+
+export default Erro404
